a0c41800f6a64c056781cdab1508e5c82249d045
knscripting <kehf@privateit.net>
2022-10-24 16:33:48 -0500

    revision

--- models/model.js
+++ models/model.js
@@ -1,7 +1,8 @@
+const { json } = require('express');
 const mongoose = require('mongoose');
 const opts = {
     // Make Mongoose use Unix time (seconds since Jan 1, 1970)
-    timestamps: { currentTime: () => Math.floor(Date.now() / 1000) },
+    timestamps: { currentTime: () => Math.floor(Date.now() ) },
   };
 
 const dataSchema = new mongoose.Schema({
@@ -14,6 +15,24 @@ const dataSchema = new mongoose.Schema({
     mmValue: {
         type: String,
         required: true
+    },
+    mmObjectName: {
+        type: String
+    },
+    mmObjectKey: {
+        type: String
+    },
+    mmOwnerKey: {
+        type: String
+    },
+    mmOwnerName: {
+        type: String
+    },
+    mmLocalPos: {
+        type: String
+    },
+    mmRegion: {
+        type: String
     }
 },opts)
 //dataSchema.set('timestamps', {currentTime: () => Math.floor(Date.now() / 1000)
--- routes/routes.js
+++ routes/routes.js
@@ -2,28 +2,46 @@ const express = require('express');
 const Model = require('../models/model');
 const router = express.Router();
 
+
+
 router.get('/', function(req, res){
     res.json({status: "online", message: "mmkvp ready" })
 })
 
+/*
+Upsert is enabled, if record doesn't exist, create it
+We record the SL header information as it could be 
+handy with debugging, and other functions
+*/
+
 router.put('/update', async (req, res) => {
     try {
-        const id = {mmKey: req.body.mmKey.toString()};
-        const updatedData = {mmKey: req.body.mmKey, mmValue: req.body.mmValue};
-        const options = { returnDocument: "after" };
+        const updatedData = {
+            mmKey: req.body.mmKey,
+            mmValue: req.body.mmValue,
+            mmObjectName: req.headers['x-secondlife-object-name'],
+            mmObjectKey: req.headers['x-secondlife-object-key'],
+            mmOwnerName: req.headers['x-secondlife-owner-name'],
+            mmOwnerKey: req.headers['x-secondlife-owner-key'],
+            mmLocalPos: req.headers['x-secondlife-local-position'],
+            mmRegion: req.headers['x-secondlife-region'] } ;
 
+        const id = {mmKey: req.body.mmKey.toString()};
+        const options = { returnDocument: "after", upsert: "true" };
         const result = await Model.findOneAndUpdate(
             id, updatedData, options
         )
-        res.send(result);
-        //console.log('query: ' + query +'update attempt: '+ updatedData.toString());
+        //res.send(result) //working
+        const sendMe = {mmValue: result.mmValue};
+        res.send(sendMe);
+        console.log("Writing:", id);
     }
     catch (error) {
         res.status(500).json({ status: "ERROR", message: error.message })
     }
 })
 
-//Post Method
+//Post Method Not needed
 router.post('/create', async (req, res) => {
     const data = new Model({
         mmKey: req.body.mmKey,
@@ -55,11 +73,12 @@ router.get('/getAll', async (req, res) => {
 router.post('/getOne', async (req, res) => {
     try {
         var id = {mmKey: req.body.mmKey};
-        console.log(id);
+        console.log("Reading:",id);
         const data = await Model.findOne(id);
         res.json(data)
     }
     catch (error) {
+        console.log("No Key");
         res.status(500).json({ status: "failed", message: error.message })
     }
 })
